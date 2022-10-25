8310c397c6f956967f23f0c0beee3d46e523b448
issf69 <irenesilvafranca2016@gmail.com>
2022-10-25 09:04:49 -0300

    Operadores.java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,116 @@
+import javax.lang.model.util.ElementScanner14;
+import javax.net.ssl.ExtendedSSLSession;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        /*
+         * String nomeCompleto = "LINGUAGEM" + "JAVA";
+         * 
+         * // System.out.println(nomeCompleto);
+         * // System.out.println(concatenacao);
+         * /*
+         * String concatenacao = "?";
+         * 
+         * concatenacao = 1 + 1 + 1 + "1";
+         * 
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1 + "1" + 1 + 1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1 + "1" + 1 + "1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1" + (1 + 1 + 1);
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1" + (1 + 1 + 1);
+         * System.out.println(concatenacao);
+         */
+        /*
+         * int numero = 5;
+         * 
+         * numero = -numero;
+         * System.out.println(numero);
+         * numero = numero * -1;
+         * System.out.println(numero);
+         * 
+         * int numero = 5;
+         * x repeticao
+         * numero = numero + 1;
+         * numero++;
+         * numero = numero +1;
+         * 
+         * System.out.println(numero++);
+         * System.out.println(++numero);
+         * 
+         * System.out.println(numero);
+         * 
+         * System.out.println(numero--);
+         * System.out.println(numero);
+         */
+        // boolean variavel = true;
+        // System.out.println(!variavel);
+        // variavel = !variavel;
+
+        // System.out.println(variavel);
+
+        // int a, b;
+        // a = 6;
+        // b = 6;
+        // String resultado = a == b ? "verdadeiro" : "falso";
+        // String resultado = a == b ? "1" : "0";
+        // if(a==b);
+        // resultado = "verdadeiro";
+        // else
+        // resultado = "falso";
+        // System.out.println(resultado);
+
+        /*
+         * String nomeUm = "IRENE";
+         * // String nomeDois = "IRENE";
+         * String nomeDois = new String("IRENE");
+         * 
+         * // System.out.println(nomeUm == nomeDois);
+         * System.out.println(nomeUm.equals(nomeDois));
+         * 
+         * int numero1 = 1;
+         * int numero2 = 2;
+         * 
+         * boolean simNao = numero1 == numero2;
+         * // if (numero1 < numero2)
+         * 
+         * if (numero1 == numero2) { // true
+         * System.out.println("a nossa condicao e verdadeira");
+         * }
+         * System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+         * 
+         * simNao = numero1 != numero2;
+         * System.out.println("numeroUm é diferente ao numeroDois? " + simNao);
+         * 
+         * simNao = numero1 > numero2;
+         * System.out.println("numeroUm é maior ao numeroDois? " + simNao);
+         */
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+        // if (condicao1 && condicao2);
+
+        // System.out.println("As duas condicoes sao verdadeiras");
+
+        if (condicao1 && (7 > 4))
+            ;
+        {
+            System.out.println("As duas condicoes sao verdadeiras");
+        }
+
+        System.out.print("fim");
+
+        if (condicao1 || condicao2)
+            ;
+
+        System.out.println("uma dass condicoes sao verdadeiras");
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Irene Silva";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numerCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        // final double VALOR DE PI = 3.14;
+
     }
 }
